c68b125504602d31ff4d10b8aff979d57a59813e
Balaji H A <dev0001@gmail.com>
2024-10-14 17:56:29 +0700

    Single Pong 0

---
+++ PongGame/public/config/game_conf.js
@@ -0,0 +1,13 @@
+module.exports = {
+    ballSpeed: 0.2,
+    canvasWidth: 400,
+    canvasHeight: 300,
+    paddlewidth: 60,
+    paddleHeight: 10,
+    yPaddlePosPerct: 90,
+    borderWidth: 10,
+    ballPaddleHeight: 10,
+    speedTimeFrame: 2,  //ms
+    ballIncrementPos: 0.5,
+    paddleMoveDist: 10
+}
